4dc2ba8a788e72eedc8223276e5ec7678d7f43ae
CarbonylGroup <carbonylgp@gmail.com>
2017-11-06 09:59:11 +0800

    formet code a little bit

--- app/src/main/java/com/carbonylgroup/schoolpower/utils/Utils.kt
+++ app/src/main/java/com/carbonylgroup/schoolpower/utils/Utils.kt
@@ -34,15 +34,27 @@ import java.util.*
 
 class Utils(private val context: Context) {
 
-    private val gradeColorIds = intArrayOf(R.color.A_score_green, R.color.B_score_green, R.color.Cp_score_yellow, R.color.C_score_orange, R.color.Cm_score_red, R.color.primary_dark, R.color.primary, R.color.primary)
-    private val gradeColorIdsPlain = intArrayOf(R.color.A_score_green, R.color.B_score_green, R.color.Cp_score_yellow, R.color.C_score_orange, R.color.Cm_score_red, R.color.primary_dark, R.color.primary)
-    private val gradeDarkColorIdsPlain = intArrayOf(R.color.A_score_green_dark, R.color.B_score_green_dark, R.color.Cp_score_yellow_dark, R.color.C_score_orange_dark, R.color.Cm_score_red_dark, R.color.primary_darker, R.color.primary_dark)
-    private fun indexOfString(searchString: String, domain: Array<String>): Int = domain.indices.firstOrNull { searchString == domain[it] } ?: -1
+    private val gradeColorIds = intArrayOf(R.color.A_score_green, R.color.B_score_green,
+            R.color.Cp_score_yellow, R.color.C_score_orange, R.color.Cm_score_red,
+            R.color.primary_dark, R.color.primary, R.color.primary)
+
+    private val gradeColorIdsPlain = intArrayOf(R.color.A_score_green, R.color.B_score_green,
+            R.color.Cp_score_yellow, R.color.C_score_orange, R.color.Cm_score_red,
+            R.color.primary_dark, R.color.primary)
+
+    private val gradeDarkColorIdsPlain = intArrayOf(R.color.A_score_green_dark,
+            R.color.B_score_green_dark, R.color.Cp_score_yellow_dark, R.color.C_score_orange_dark,
+            R.color.Cm_score_red_dark, R.color.primary_darker, R.color.primary_dark)
+
+    private fun indexOfString(searchString: String, domain: Array<String>):
+            Int = domain.indices.firstOrNull { searchString == domain[it] } ?: -1
 
     /* Color Handler */
-    fun getColorByLetterGrade(context: Context, letterGrade: String) = ContextCompat.getColor(context, gradeColorIds[indexOfString(letterGrade, arrayOf("A", "B", "C+", "C", "C-", "F", "I", "--"))])
+    fun getColorByLetterGrade(context: Context, letterGrade: String) = ContextCompat.getColor(context,
+            gradeColorIds[indexOfString(letterGrade, arrayOf("A", "B", "C+", "C", "C-", "F", "I", "--"))])
 
-    fun getDarkColorByPrimary(originalPrimary: Int) = ContextCompat.getColor(context, gradeDarkColorIdsPlain[gradeColorIdsPlain.takeWhile { originalPrimary != ContextCompat.getColor(context, it) }.count()])
+    fun getDarkColorByPrimary(originalPrimary: Int) = ContextCompat.getColor(context,
+            gradeDarkColorIdsPlain[gradeColorIdsPlain.takeWhile { originalPrimary != ContextCompat.getColor(context, it) }.count()])
 
     /* Others */
     fun dpToPx(dp: Int) = Math.round(dp * (context.resources.displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT))
